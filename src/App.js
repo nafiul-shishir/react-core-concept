b4b6bc2cd61fa939dc3f786373d51ea7b4e17f16
nafiul-shishir <nafiul.shishir@gmail.com>
2022-01-09 08:40:36 +0600

    Understanding About component now complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
 function App() {
+  
+  const nayok = ['Shakib','Tamim', 'Mushfiq','Nakib','Mashrafe','Ashraful','Rubel','Mehedi'];
+  const products = [
+    {name: 'Photoshop', price: '$9.90/mo'},
+    {name: 'Illustrator', price: '$6.60'},
+    {name: 'PDF Reader', price: '$3.33'}
+  ]
+
+  const nayokName = nayok.map(nayoks => nayoks.name)
+  console.log(nayokName);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ProductList name={products[0].name} price={products[0].price} ></ProductList>
+        
+        
+      
+        
       </header>
     </div>
   );
 }
+
+function ProductList(props){
+  const ProductStyle = {
+    border: "2px Solid #4FBDBA",
+    borderRadius: "5px",
+    Padding:"",
+    backgroundColor: "lightgray",
+    height: '300px',
+    width: '300px',
+    float: "left"
+
+  }
+  return(
+    <div style={ProductStyle}>
+      <h3> {props.name} </h3>
+      <h1> {props.price} </h1>
+      
+      <button style={{backgroundColor:"#1C6DD0", padding:"5px", borderRadius:"18px" }}> Buy Now </button>
+    </div>
+  )
+}
+
+
 
 export default App;
