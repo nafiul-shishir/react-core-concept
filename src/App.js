32934032c6e66ac11c4ee34bbecca7c216b3bbf6
nafiul-shishir <nafiul.shishir@gmail.com>
2022-01-09 14:58:02 +0600

    learn comment, useState & also useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
+
+  
   
   const nayok = ['Shakib','Tamim', 'Mushfiq','Nakib','Mashrafe','Ashraful','Rubel','Mehedi'];
   const products = [
@@ -11,13 +13,20 @@ function App() {
     {name: 'PDF Reader', price: '$3.33'}
   ]
 
-  const nayokName = nayok.map(nayoks => nayoks.name)
-  console.log(nayokName);
+  
 
 
   return (
     <div className="App">
       <header className="App-header">
+      <Counter></Counter>
+      <Posts></Posts>
+      
+        <ul>
+          { nayok.map(nayoksname => <li> {nayoksname}</li>)}
+         
+        </ul>
+        {products.map(pd => <ProductList>product={pd}</ProductList>)}
         <ProductList name={products[0].name} price={products[0].price} ></ProductList>
         
         
@@ -26,6 +35,19 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0)
+  const handleIncrease =() => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1)
+  return(
+    <div>
+      <h1> Count : {count} </h1>
+      <button onClick={handleDecrease}> decrease </button>
+      <button onClick={handleIncrease}> Increase </button>
+    </div>
+  )
 }
 
 function ProductList(props){
@@ -41,10 +63,31 @@ function ProductList(props){
   }
   return(
     <div style={ProductStyle}>
-      <h3> {props.name} </h3>
-      <h1> {props.price} </h1>
+      <h3>  {props.name} </h3>
+      <h1>  {props.price} </h1>
       
       <button style={{backgroundColor:"#1C6DD0", padding:"5px", borderRadius:"18px" }}> Buy Now </button>
+    </div>
+  )
+}
+
+function Posts(){
+  const [post,setPost] = useState([])
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPost(data))
+  },[])
+
+  return(
+    <div>
+      <h1> Numbers Of People Post: {post.length} </h1>
+      <h4> 
+        {
+          post.map(posts => <p> {posts.id}, {posts.title}</p>)
+        }
+
+      </h4>
     </div>
   )
 }
